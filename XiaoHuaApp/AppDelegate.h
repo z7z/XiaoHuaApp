860509646a04aa6ae6f1fadfd4c869bed1687a80
z7z <Erichow@zhoushiyedeMacBook-Air.local>
2014-07-09 21:07:33 +0800

    Initial Commit

---
+++ XiaoHuaApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XiaoHuaApp
+//
+//  Created by 周仕野 on 14-7-9.
+//  Copyright (c) 2014年 GYT.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
